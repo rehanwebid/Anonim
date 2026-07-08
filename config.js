49548d782597ac00757a3ca2b6d095667edeaba1
rehanwebid <rehanwebid@gmail.com>
2026-07-08 22:58:21 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// ============================================
+// ⚙️ KONFIGURASI API
+// ============================================
+const API_URL = 'https://script.google.com/macros/s/AKfycbxETY9QM9jemUH92wDQ7ovaxaER_DrR2cuQYuRFCnNEmF9KBMvaRL-iF96FQ5A2P0nJ/exec';
