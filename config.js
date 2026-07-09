554794978323591aa50163f865412760785f29c3
rehanwebid <rehanwebid@gmail.com>
2026-07-09 21:26:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // ============================================
-// ⚙️ KONFIGURASI API
+// ⚙️ KONFIGURASI
 // ============================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbxETY9QM9jemUH92wDQ7ovaxaER_DrR2cuQYuRFCnNEmF9KBMvaRL-iF96FQ5A2P0nJ/exec';
+const API_URL = 'https://script.google.com/AKfycbxETY9QM9jemUH92wDQ7ovaxaER_DrR2cuQYuRFCnNEmF9KBMvaRL-iF96FQ5A2P0nJ/exec';
+const GOOGLE_CLIENT_ID = '71225616950-3kbgu72h3gbrg0ogfnsgvss204moh1r9.apps.googleusercontent.com';
