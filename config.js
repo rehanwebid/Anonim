ff10128d8edc9c7823e0cd1c5b68d41048d8ec9b
rehanwebid <rehanwebid@gmail.com>
2026-07-10 19:57:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-// ============================================
-// ⚙️ KONFIGURASI
-// ============================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbxED3EglZCDfm9vn2bdaruTr8th0ZV_lBA-tQVvulK0qDlCC_XhPMrZ8VMW9RBb14bU/exec';
-const GOOGLE_CLIENT_ID = '71225616950-3kbgu72h3gbrg0ogfnsgvss204moh1r9.apps.googleusercontent.com';
+
